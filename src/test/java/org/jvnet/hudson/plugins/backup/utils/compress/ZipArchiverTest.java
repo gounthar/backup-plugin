a80b5ae222905f5ce01effa75ee341ba19123239
Bruno Verachten <gounthar@gmail.com>
2025-07-11 22:59:11 +0200

    fix(test): add detailed diagnostics for directory content mismatches in ZIP extraction test

--- src/test/java/org/jvnet/hudson/plugins/backup/utils/compress/ZipArchiverTest.java
+++ src/test/java/org/jvnet/hudson/plugins/backup/utils/compress/ZipArchiverTest.java
@@ -25,6 +25,8 @@
 package org.jvnet.hudson.plugins.backup.utils.compress;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.codehaus.plexus.archiver.zip.ZipUnArchiver;
@@ -56,13 +58,46 @@ public class ZipArchiverTest {
 		targetDirectory.mkdir();
 	}
 
+	private void printDirectoryDiff(File expected, File actual) {
+		Set<String> expectedFiles = new HashSet<>();
+		Set<String> actualFiles = new HashSet<>();
+		for (File f : expected.listFiles())
+			expectedFiles.add(f.getName());
+		for (File f : actual.listFiles())
+			actualFiles.add(f.getName());
+		for (String name : expectedFiles) {
+			if (!actualFiles.contains(name)) {
+				System.err.println("Missing in extracted: " + name);
+			}
+		}
+		for (String name : actualFiles) {
+			if (!expectedFiles.contains(name)) {
+				System.err.println("Extra in extracted: " + name);
+			}
+		}
+		for (File f : expected.listFiles()) {
+			File af = new File(actual, f.getName());
+			if (af.exists() && f.isFile() && af.isFile()) {
+				try {
+					if (!org.apache.commons.io.FileUtils.contentEquals(f, af)) {
+						System.err.println("File content differs: " + f.getName());
+					}
+				} catch (Exception e) {
+					System.err.println("Error comparing files: " + f.getName());
+				}
+			} else if (af.exists() && f.isDirectory() && af.isDirectory()) {
+				printDirectoryDiff(f, af);
+			}
+		}
+	}
+
 	@Test
 	public void testArchiver() throws Exception {
 		ZipArchiver archiver = new ZipArchiver();
 		archiver.init(archiveFile);
 
 		ArchiverTestUtil.addTestFiles(archiver);
-		
+
 		ZipUnArchiver unarchiver = new ZipUnArchiver();
 		unarchiver.setSourceFile(archiveFile);
 
@@ -71,13 +106,13 @@ public class ZipArchiverTest {
 				"Logger"));
 		unarchiver.extract();
 
-		boolean result = ArchiverTestUtil.compareDirectoryContent(new File(Thread
-				.currentThread().getContextClassLoader().getResource("data")
-				.getFile()), targetDirectory);
+		File expectedDir = new File(Thread.currentThread().getContextClassLoader().getResource("data").getFile());
+		boolean result = ArchiverTestUtil.compareDirectoryContent(expectedDir, targetDirectory);
 		if (!result) {
 			System.err.println("Directory content mismatch after ZIP extraction.");
-			System.err.println("Reference: " + new File(Thread.currentThread().getContextClassLoader().getResource("data").getFile()).getAbsolutePath());
+			System.err.println("Reference: " + expectedDir.getAbsolutePath());
 			System.err.println("Extracted: " + targetDirectory.getAbsolutePath());
+			printDirectoryDiff(expectedDir, targetDirectory);
 		}
 		Assert.assertTrue(result);
 	}
